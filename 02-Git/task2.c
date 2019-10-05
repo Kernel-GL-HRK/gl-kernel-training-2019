1f030953c6a5582bb5ab2dd60306699c5b152ec2
Dmitry Domnin <dmitry8728@gmail.com>
2019-10-05 21:13:19 +0300

    02-Git: Add comparison logic
    Signed-off-by: Dmitry Domnin <dmitry8728@gmail.com>

--- 02-Git/task2.c
+++ 02-Git/task2.c
@@ -16,5 +16,21 @@ int main (int argc, char *argv[])
 		case 3: out = 's'; break;
 	}
 
+	if(in == out)
+		printf("> Our choice is the same\n> Draw\n");
+	else if(in == 'r' && out == 'p')
+                printf("> You choose rock, I choose paper\n> I win: paper beats rock\n");
+	else if(in == 'r' && out == 's')
+                printf("> You choose rock, I choose scissors\n> You win: rock beats scissors\n");
+	else if(in == 'p' && out == 'r')
+                printf("> You choose paper, I choose rock\n> You win: paper beats rock\n");
+	else if(in == 'p' && out == 's')
+                printf("> You choose paper, I choose scissors\n> I win: scissors cuts paper\n");
+	else if(in == 's' && out == 'r')
+                printf("> You choose scissors, I choose rock\n> I win: rock beats scissors\n");
+	else if(in == 's' && out == 'p')
+                printf("> You choose scissors, I choose paper\n> You win: scissors cuts paper\n");
+	else printf("> Your choice is wrong\n");
+
 	return 0;
 }
