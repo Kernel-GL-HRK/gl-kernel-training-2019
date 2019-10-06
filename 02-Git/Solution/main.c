9b6f31e65d30e0ebebaddd2d519ed4032f1ce01d
Happybol <dmytro.topikha@gmail.com>
2019-10-06 19:34:31 +0300

    02-GIT:	Init prj Build and print greeting.
    Signed-off-by: Happybol <dmytro.topikha@gmail.com>

---
+++ 02-Git/Solution/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "string.h"
+int main(){
+    printf("Welcome to the console game 'rock-paper-scissors'\n");
+    
+    return 0;
+} 
