40a58e1c146a6a59e1eb8e36a59677fa79479f9f
Vladyslav <omelya1998chirk@gmail.com>
2019-10-07 13:47:17 +0300

    02-Git: Implement of winner determination logic
    Signed-off-by: Vladyslav <omelya1998chirk@gmail.com>

--- 02-Git/solution/main.c
+++ 02-Git/solution/main.c
@@ -11,6 +11,9 @@ enum elements_of_game {
 };
 
 
+const enum elements_of_game mask_winner[3] = {PAPER, ROCK, SCISSORS};
+
+
 const char* element_to_str(enum elements_of_game e_o_g)
 {
 	const char* const array_elements[] = {"rock", "scissors", "paper"};
@@ -19,10 +22,10 @@ const char* element_to_str(enum elements_of_game e_o_g)
 
 char wait_and_get_user_input(void)
 {
-	char user_select;
+	char user_selected;
 	printf("Please choose: rock (r) - paper (p) - scissors (s)\n");
-	scanf("%c", &user_select);
-	return user_select;
+	scanf("%c", &user_selected);
+	return user_selected;
 }
 
 
@@ -49,19 +52,31 @@ enum elements_of_game generate_random_element(void)
 
 
 
+void determinat_winner(enum elements_of_game pc, enum elements_of_game man)
+{
+	if (pc == man)
+		printf("Draw\n");
+	else if (mask_winner[pc] == man)
+		printf("Man win\n");
+	else
+		printf("Computer win\n");
+}
+
+
+
 int main(void)
 {
 
 	char element = wait_and_get_user_input();
-	enum elements_of_game user_select =  char_convert_to_element(element);
-	if (user_select == INCOR_VAL)
+	enum elements_of_game user_selected =  char_convert_to_element(element);
+	if (user_selected == INCOR_VAL)
 		printf("Invalid user input\n");
 	else
-		printf("Man choose %s\n", element_to_str(user_select));
+		printf("Man choose %s\n", element_to_str(user_selected));
 
 		enum elements_of_game pc_choice = generate_random_element();
-
 		printf("Computer chose %s\n", element_to_str(pc_choice));
+		determinat_winner(pc_choice, user_selected);
 	return 0;
 
 }
