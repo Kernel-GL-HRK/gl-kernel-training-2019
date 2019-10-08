5afe56975edb85897979a04617204e32cbaae88a
Happybol <dmytro.topikha@gmail.com>
2019-10-08 21:44:03 +0300

    02-GIT: Add statistics Statistics show the result of the game in 1 game session ( common number win / lose).
    Signed-off-by: Happybol <dmytro.topikha@gmail.com>

--- 02-Git/Solution/main.c
+++ 02-Git/Solution/main.c
@@ -8,6 +8,7 @@ int main(){
     srand(time(NULL));
     const char roll[] = {'r','p','s'};
     char choose_pc[10];
+    unsigned int stat[2];
     while(1){
         printf("Please choose: rock (r) - paper (p) - scissors (s)\n");
         scanf("%s",symbol);
@@ -16,28 +17,47 @@ int main(){
             case 'r':
                 printf("You choose 'r', I choose %s \n",(char*)&choose_pc);
                 if('p' == choose_pc[0])
+                {
                     printf("===========You lose===========\n");
+                    stat[0]++;
+                }
                 else
+                {
                     printf("===========You win===========\n");
+                    stat[1]++;
+                }
                 break;
             case 'p':
                 printf("You choose 'p', I choose %s \n",(char*)&choose_pc);
                 if('s' == choose_pc[0])
+                {
                     printf("===========You lose===========\n");
+                    stat[0]++;
+                }
                 else
+                {
                     printf("===========You win===========\n");
+                    stat[1]++;
+                }
                 break;
             case 's':
                 printf("You choose 's', I choose %s \n",(char*)&choose_pc);
                 if('r' == choose_pc[0])
+                {
                     printf("===========You lose===========\n");
+                    stat[0]++;
+                }
                 else
+                {
                     printf("===========You win===========\n");
+                    stat[1]++;
+                }
                 break;
             default:
                 printf("Error\n");
                 continue;
         }
-        memset(symbol,0,sizeof(symbol));
+        printf("Statistic: You win: %d --- You lose %d\n",stat[1],stat[0]);
+        printf(" \n New Game \n");
     }
 }
