2c9857da8e13c094d00915e57c69d1e30a51f3b4
Anton <ad742452@gmail.com>
2019-10-09 07:44:04 +0300

    02-Git: Add user's choise realization
    Function which request and get information about user's choice
    through console was added. Also enumeration of game object was added.
    
    Signed-off-by: Anton <ad742452@gmail.com>

--- 02-Git/main.c
+++ 02-Git/main.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
 
+// PRS enumeration
+enum PRS
+{
+	PRS_PAPER = 0,
+	PRS_ROCK = 1,
+	PRS_SCISSORS = 2
+};
+
+// function prototypes
+int getUserChoice();
+
+// main function
 int main()
 {
-	printf("Hello, world!\n");
+	int userChoise = getUserChoice();
 
+	//
 	return 0;
+}
+
+// get user choice funtion
+int getUserChoice()
+{
+	int choise;
+
+	printf("Enter your choice (0-paper, 1-rock, 2-scissors) > ");
+	fscanf(stdin, "%d", &choise);
+		
+	// result
+	return choise;
 }
 
