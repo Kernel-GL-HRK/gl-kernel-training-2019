b081cfa75407f338eee2674abb64e2ee7544f094
Andrey Pahomov <pahomov.and@gmail.com>
2019-10-12 19:58:36 +0300

    03-Bash: Add checking of user id from passwd and get the id, grup, home dir of user
    Signed-off-by: Andrey Pahomov <pahomov.and@gmail.com>

--- 03-Bash/fix_owner/fix_owner.sh
+++ 03-Bash/fix_owner/fix_owner.sh
@@ -23,4 +23,45 @@ parse_args(){
 	done
 }
 
+get_user(){
+	echo $1 | cut -d\: -f 1
+}
+
+get_id(){
+	echo $1 | cut -d\: -f 3
+}
+
+get_grup(){
+	echo $1 | cut -d\: -f 4
+}
+
+get_home_dir(){
+	echo $1 | cut -d\: -f 6
+}
+
+user_validation(){
+	if [  $1 -eq 1000 ] || [ $1 -gt 1000 ];then
+		return 0
+	fi
+	return 1
+ }
+
+##########_MAIN_#########
+
 parse_args $@
+
+if [ "$_help" = true ]; then
+	help
+	exit 0
+fi
+
+while read -r line;do
+	USER=$(get_user $line)
+	ID=$(get_id $line)
+	GRUP=$(get_grup $line)
+	HOME_DIR=$(get_home_dir $line)
+
+	user_validation $ID
+	if [  $? -eq 0 ]; then
+	fi
+done < /etc/passwd
