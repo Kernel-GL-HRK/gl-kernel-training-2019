7f82cdfc11f00f9f8e0678139891105f0eed7eae
bekir <bekir.bekirov@l1inc.com>
2019-10-13 20:34:00 +0300

    03-Bash_find_dup_files: Add info about size of file
    Signed-off-by: bekir <bekir.bekirov@l1inc.com>

--- 03-Bash/find_dup_files/find_dup_files.sh
+++ 03-Bash/find_dup_files/find_dup_files.sh
@@ -1,7 +1,6 @@
 ! #bin/bash
 
 if [[ $1 = "-h" || $1 = "--help" ]]; then
-    HELP_FLAG=1
     echo "Program prints file names and file size for files with same md5 checksum"
     exit 0
 else
@@ -10,24 +9,13 @@ else
     else     
         DIR=$1
     fi
-HELP_FLAG=0
 fi
 
 if [[ -d "$DIR" ]]; then
-    touch -c temp_hash_file.md5
-    :> temp_hash_file.md5
-    LIST_FILES=$(find $DIR -maxdepth 1 -type f)
-        for item in $LIST_FILES; do
-            md5sum "$item" >> temp_hash_file.md5
-        done
-	UNIQ_FILES=$(uniq -D -w 32 temp_hash_file.md5 | sed 's/\s\+/:/' | cut -d : -f 2)
-        for item in $UNIQ_FILES; do
-            NAME=$(ls -s $item | sed 's/\s\+/,/' | cut -d , -f 2)
-            SIZE=$(ls -s $item | sed 's/\s\+/,/' | cut -d , -f 1)
-            echo "$NAME $SIZE"
-        done
-    rm -f temp_hash_file.md5
+    find $DIR -maxdepth 1 -type f -exec md5sum {} \; | sort | uniq -D -w 32 | tr -s ' ' | cut -d ' ' -f 2- | \
+    while read line; do
+        echo "$line" | sed 's/ /\\ /g' | xargs ls -lh | tr -s ' ' | cut -d ' ' -f 5,9-
+    done
 else
     echo "Fail! This isn't dir"
 fi
-
