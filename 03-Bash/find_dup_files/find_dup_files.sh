40f3df3cb3cf7192a29f313aad6c68c493bdcf35
Dmitry Domnin <dmitry8728@gmail.com>
2019-10-14 11:34:43 +0300

    03-Bash: Add script information
    Signed-off-by: Dmitry Domnin <dmitry8728@gmail.com>

--- 03-Bash/find_dup_files/find_dup_files.sh
+++ 03-Bash/find_dup_files/find_dup_files.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
 DIR="$(pwd)"
-FILE="file.txt"
 
 if [ -n "$1" ]; then
-	DIR="$1"
+	case "$1" in
+		-h | --help)
+			echo "Usage: find_dup_files.sh [OPTION] or [DIR]"
+			echo -e "List information about the FILEs with identical md5 (the current directory by default).\n"
+			echo "Options (no more than one argument):"
+			echo -e "	-h, --help     display this help and exit\n"
+			exit
+		;;
+		*) DIR="$1"
+		;;
+	esac
 fi
 
 echo "Search in $DIR"
-find "$DIR" -maxdepth 1 -type f -printf "size:%s\t" -exec md5sum {} \; > "$FILE"
-sort -k2 "$FILE" | uniq -f1 -w32 -D
-
+find "$DIR" -maxdepth 1 -type f -printf "size:%s\t" -exec md5sum {} \; | sort -k2 | uniq -f1 -w32 -D
