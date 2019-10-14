4d7f070d880129039a1eacc47c19c983b1448d81
Vitalii Pudov <pudov@ukr.net>
2019-10-14 19:53:28 +0300

    03-Bash: Add solution for print the quantity files
    Bash script was implemented: Print the quantity of files with extention .c, .cpp and *.py. for target directory
    
    Signed-off-by: Vitalii Pudov <pudov@ukr.net>

--- 03-Bash/kernel_git/kernel_hm.sh
+++ 03-Bash/kernel_git/kernel_hm.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
 
+display_help() {
+  echo "Usage: $0 [target directory]"
+  echo
+}
+
+if [ "$#" -ne 1 ] || [ "$1" = "--help" ]; then
+  display_help
+  exit 1
+fi
+
+if [ ! -d "$1" ]; then
+  echo target directory does not exists
+  exit 1
+fi
+
+cd $1
+echo Quantity of files with extention .c in target diretory:
+ls -lR | grep --count \.c$
+echo Quantity of files with extention .cpp in target diretory:
+ls -lR | grep --count \.cpp$
+echo Quantity of files with extention .py in target diretory:
+ls -lR | grep --count \.py$
+
+echo
+echo name of executed script:
 echo $0
