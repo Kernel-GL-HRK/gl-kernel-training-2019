0852274302e07a21688feab7d34a055326613a6c
Vitalii Pudov <v.pudov@vizaar.com>
2019-10-15 11:59:34 +0300

    03-Bash: Add check home solution
    Solution was implemented: For every user in system with id >= 1000 check home folder.
    
    Signed-off-by: Vitalii Pudov <v.pudov@vizaar.com>

--- 03-Bash/fix_owner/kernel_hm.sh
+++ 03-Bash/fix_owner/kernel_hm.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+
+# For every user in system with id >= 1000 check home folder.
+
+local_users=($( awk -F: '($3 >= 1000) {printf "%s\n",$1}' /etc/passwd))
+
+for local_user in "${local_users[@]}";
+do
+  echo $local_user
+  user_path=$(grep $local_user /etc/passwd|cut -f6 -d":")
+  if [  -d "$user_path"  ];
+    then
+      echo "$user_path direcroty is present"
+    else
+      echo "$user_path direcroty is present"
+  fi
+done
 
 echo
 echo name of executed script:
