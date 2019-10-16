ad47c2f162a93a5ca2faf3b6eb44494c5d939707
Chekanov Serhii <chekanovserhii@gmail.com>
2019-10-16 18:20:57 +0300

    03-Bash: Add count feature
    Count feature with ls -lr comands is done.
    Script Kernel stats done.
    
    Signed-off-by: Chekanov Serhii <chekanovserhii@gmail.com>

--- 03-Bash/kernel_git/kernel_stats.sh
+++ 03-Bash/kernel_git/kernel_stats.sh
@@ -10,3 +10,21 @@ if [[( "$#" -ne 1 ) || ( "$1" = "--help" )]]; then
   stats_help
   exit 1
 fi
+
+# Quantity of files with extention .c. 
+echo Quantity of .c files in diretory
+find $(pwd) -type f -name '*.c' | wc -l
+# Quantity of files with extention .cpp.
+echo Quantity of .cpp files in diretory
+find $(pwd) -type f -name '*.cpp' | wc -l
+# Quantity of files with extention .py.
+echo Quantity of .py files in diretory
+find $(pwd) -type f -name '*.py' | wc -l
+
+# Quantity of commits with word 'revert'.
+echo Quantity of commits with word "\'revert\'"
+git rev-list HEAD --count --grep='revert'
+
+# Lines for each author are in .py files.
+find -name "*.py" -exec \
+git blame --line-porcelain {} \; | sed -n 's/^author //p' | sort | uniq -c | sort -rn
