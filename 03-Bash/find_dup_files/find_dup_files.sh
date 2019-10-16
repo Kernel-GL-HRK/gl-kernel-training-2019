b3f4533c276e977a0743a08bdaf79e03fb651e83
Vitalii Pudov <pudov@ukr.net>
2019-10-17 01:13:02 +0300

    03-Bash: Add solution
    Print file names and file size for files with same md5 checksum.
    
    Signed-off-by: Vitalii Pudov <pudov@ukr.net>

--- 03-Bash/find_dup_files/find_dup_files.sh
+++ 03-Bash/find_dup_files/find_dup_files.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
+display_help() {
+  echo "Usage: $0 [target directory]"
+  echo
+  echo "    -h, --help - print these options"
+  echo
+}
+
+declare DYR_PATH
+
+if [ "$#" -eq 0 ]; then
+  DYR_PATH="."
+else
+  if [ "$1" = "--h" ] || [ "$1" = "--help" ]; then
+    display_help
+    exit 1
+  else
+    DYR_PATH="$1"
+  fi
+fi
+
+for pfile in $(find $DYR_PATH -type f -name "*");
+do
+  echo $pfile -
+  MDSUM=($(md5sum $pfile))
+  if [ ! -z "$MDSUM" ]; then
+    for sfile in $(find $DYR_PATH -type f -exec md5sum {} + | grep "^$MDSUM");
+    do
+      if [ $sfile != $pfile ] && [ -f "$sfile" ]; then
+        echo $MDSUM :
+        echo size of $sfile \file is $(stat -c%s "$sfile")
+      fi
+    done
+  fi
+done
+
 echo
 echo name of executed script:
 echo $0
