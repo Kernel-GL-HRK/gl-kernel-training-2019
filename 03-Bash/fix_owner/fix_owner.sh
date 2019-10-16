a60be0cf8cee884386e243fe3623c93089f3e59e
Chekanov Serhii <chekanovserhii@gmail.com>
2019-10-16 13:00:59 +0300

    03-Bash: Add all script
    Final script with change owner done.
    
    Signed-off-by: Chekanov Serhii <chekanovserhii@gmail.com>

--- 03-Bash/fix_owner/fix_owner.sh
+++ 03-Bash/fix_owner/fix_owner.sh
@@ -15,3 +15,54 @@ function Help()
     echo "-h, - print help options."
     exit 0
 }
+
+# check options for script
+if [ $# -ne 0 ]; then
+    case $1 in 
+        -h) Help;; 
+        -f) fix_force=1;;
+        *) exit 1;;
+    esac
+fi
+
+for user_line in $(cat $user_file)
+do
+    declare -i user_id
+    user_id=$(echo $user_line | cut -d ':' -f3)
+    if [[ ($user_id >= 1000) && ($user_id <= 60000) ]]; then
+		find $user_home -maxdepth 1 > temp_file
+	    temp_file="homeFileList"
+		continue
+    fi
+#collect about user
+	user_name=$(echo $user_line | cut -d ':' -f1)
+    user_home=$(echo $user_line | cut -d ':' -f6)
+    user_group=$(echo $user_line | cut -d ':' -f4)
+
+    for homeFile in $(cat $temp_file)
+    do
+      fileOwner=$(stat -c '%U' $homeFile)
+      if [[ "$fileOwner" != "$user_name" ]]; then
+          echo "$homeFile belong root"
+          echo "appropriate?"
+          echo -n "Y/N :"
+          if [ $force -eq 1 ]
+          then
+              decision="Y"
+              echo "forced"
+          else
+                read decision
+          fi
+          if [[ $decision == "Y" ]]; then
+              echo "change owner $user_name $homeFile"
+              sudo chown $user_name $homeFile
+              if [ $(stat -c '%U' $homeFile) = "$user_name" ];then
+                  echo "assign succes"
+              else
+                  echo "assign fail"
+              fi
+          fi
+      fi
+  done
+done
+rm ${temp_file} 
