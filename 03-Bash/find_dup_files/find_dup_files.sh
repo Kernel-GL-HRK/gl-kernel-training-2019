a5c7e6c1032cef25b2171acb64f02593bf9ccfc7
Vladyslav <omelya1998chirk@gmail.com>
2019-10-23 17:23:06 +0300

    03-Bash: Correction of line length. Delete blank lines.
    Signed-off-by: Vladyslav <omelya1998chirk@gmail.com>

--- 03-Bash/find_dup_files/find_dup_files.sh
+++ 03-Bash/find_dup_files/find_dup_files.sh
@@ -2,41 +2,51 @@
 
 
 current_dir="$(pwd)"
-
+PARAMS=""
 function view_help {
 	echo "Script find_dup_files.sh usage information."
 	echo "SYNOPSIS"
 	echo "find_dup_files.sh [DIR] [-h]"
-	echo -e "Display information about files with identical md5, if the directory is not specified, then the current directory is by default.\n"
+	echo "Display information about files with identical md5,"
+	echo "if the directory is not specified,"
+	echo -e "then the current\n directory is by default.\n"
 	echo "Options:"
 	echo -e "\t -h, --help     display this help and exit\n"
-	exit 0
 }
 
 
 function find_similar_files {
-
-	echo "Found in the $current_dir"
-	find "$current_dir" -maxdepth 1  -printf "size:%s\t"  -type f  -exec md5sum {} \; | sort -k2 | uniq -w32 -f1  -D
-
+	echo "Found in the directory $1"
+	find "$1" -maxdepth 1  -type f -printf "size:%s\t" \
+	 -exec md5sum {} \; | sort -k2 | uniq -w32 -f1  -D
 }
 
 function wrong_option {
-
 	echo "Wrong option"
 }
 
-if [[ -n "$1" ]];then
-	case "$1" in
-		--help | -h) view_help 
-		;;
-		*) 
-		wrong_option
-		;;
-	esac
+while (( "$#" )); do
+  case "$1" in
+    --help | -h)
+      view_help
+      exit 0
+      ;;
+    --)
+      shift 1
+#      break
+      ;;
+    -*|--*=)
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *)
+      current_dir=$1
+      echo "current_dir=$current_dir"
+      break
+      ;;
+  esac
+done
 
-fi
+
 
 find_similar_files $current_dir
-
-
