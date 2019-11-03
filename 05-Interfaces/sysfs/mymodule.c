d6d9a2b31ac7217ea52e4b1675e7e3f34db6f6ed
Dmitry Domnin <dmitry8728@gmail.com>
2019-11-03 22:04:38 +0200

    05-Interfaces: Sysfs module is finished
    Signed-off-by: Dmitry Domnin <dmitry8728@gmail.com>

--- 05-Interfaces/sysfs/mymodule.c
+++ 05-Interfaces/sysfs/mymodule.c
@@ -9,37 +9,85 @@
 #define CONV_FILE_NAME	"string-conv"
 #define LOG_FILE_NAME	"log"
 
-static struct class *attr_class = 0;
+static struct class *attr_class;
+static char msg[PAGE_SIZE];
+static ssize_t totalchar, totalwrite, totalread, totalcharconv, totaldigit;
 
-static ssize_t rw_show(struct class *class, struct class_attribute *attr,
+static ssize_t conv_show(struct class *class, struct class_attribute *attr,
 	char *buf)
 {
 	int ret = 0;
+
 	pr_info("mymodule-sysfs: read file: %s\n", attr->attr.name);
+
+	if (msg[0]) {
+		ret = sprintf(buf, "%s\n", msg);
+		msg[0] = '\0';
+	}
+	totalread++;
 
 	return ret;
 }
 
-static ssize_t rw_store(struct class *class, struct class_attribute *attr,
+static ssize_t conv_store(struct class *class, struct class_attribute *attr,
 	const char *buf, size_t count)
 {
+	int i = 0;
+
 	pr_info("mymodule-sysfs: write file: %s\n", attr->attr.name);
-	pr_info("mymodule-sysfs: write count bytes: %d\n", count);
+	pr_info("mymodule-sysfs: write count bytes: %ld\n", count);
+
+	sscanf(buf, "%s", msg);
+	msg[count] = '\0';
+	totalwrite++;
+	pr_info("mymodule-sysfs: string: %s\n", msg);
+	while (msg[i] != '\0') {
+		if (isdigit(msg[i])) {
+			totaldigit++;
+		} else if (isupper(msg[i])) {
+			msg[i] = tolower(msg[i]);
+			totalcharconv++;
+		}
+		i++;
+	}
+	totalchar += i;
+	pr_info("mymodule-sysfs: to uppercase converter: %s\n", msg);
 
 	return count;
+}
+
+static ssize_t log_show(struct class *class, struct class_attribute *attr,
+	char *buf)
+{
+	int ret = 0;
+
+	pr_info("mymodule-sysfs: read file: %s\n", attr->attr.name);
+
+	ret = sprintf(buf, "Total chars puts: %ld\n", totalchar);
+	ret += sprintf(buf + ret, "Total digits puts: %ld\n",
+		totaldigit);
+	ret += sprintf(buf + ret, "Total chars to convertions: %ld\n",
+		totalcharconv);
+	ret += sprintf(buf + ret,
+		"Total number of write file \"%s\": %ld\n",
+		CONV_FILE_NAME, totalwrite);
+	ret += sprintf(buf + ret,
+		"Total number of read file \"%s\": %ld\n",
+		CONV_FILE_NAME, totalread);
+
+	return ret;
 }
 
 /*CLASS_ATTR_RW(rw);*/
 struct class_attribute class_attr_sconv = {
 	.attr = { .name = CONV_FILE_NAME, .mode = 0666 },
-	.show	= rw_show,
-	.store	= rw_store
+	.show	= conv_show,
+	.store	= conv_store
 };
 
 struct class_attribute class_attr_log = {
-	.attr = { .name = LOG_FILE_NAME, .mode = 0666 },
-	.show	= rw_show,
-	.store	= rw_store
+	.attr = { .name = LOG_FILE_NAME, .mode = 0444 },
+	.show	= log_show,
 };
 
 static int __init init_mod(void)
@@ -48,21 +96,24 @@ static int __init init_mod(void)
 
 	attr_class = class_create(THIS_MODULE, CLASS_NAME);
 	if (attr_class == NULL) {
-		pr_err("mymodule-sysfs: error creating sysfs class\n");
+		pr_err("mymodule-sysfs: error creating sysfs class: %s\n",
+			CLASS_NAME);
 		return -ENOMEM;
 	}
 
 	ret = class_create_file(attr_class, &class_attr_sconv);
 	if (ret) {
-		pr_err("mymodule-sysfs: error creating sysfs class attribute %s\n", CONV_FILE_NAME);
+		pr_err("mymodule-sysfs: error creating sysfs class attribute %s\n",
+			CONV_FILE_NAME);
 		class_destroy(attr_class);
 		return ret;
 	}
 
 	ret = class_create_file(attr_class, &class_attr_log);
 	if (ret) {
-		pr_err("mymodule-sysfs: error creating sysfs class attribute %s\n", LOG_FILE_NAME);
-		class_remove_file(attr_class, &class_attr_sconv);	
+		pr_err("mymodule-sysfs: error creating sysfs class attribute %s\n",
+			LOG_FILE_NAME);
+		class_remove_file(attr_class, &class_attr_sconv);
 		class_destroy(attr_class);
 		return ret;
 	}
