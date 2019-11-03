5c56809f43e6d50ffe812da2c76e20b3a0283f1e
Vitalii Pudov <pudov@ukr.net>
2019-11-03 18:27:59 +0200

    05-Interfaces: Add calls statistics
    Attribute for total calls was added.
    
    Signed-off-by: Vitalii Pudov <pudov@ukr.net>

--- 05-Interfaces/procfs/uppercase_conv_module.c
+++ 05-Interfaces/procfs/uppercase_conv_module.c
@@ -6,13 +6,28 @@
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
 
+#define DIR_NAME	"uc_conv_driver"
+#define CONVETER_NAME	"uc_converter"
+#define NCALLS_NAME	"num_calls"
+#define NPROCESSED_CH_NAME	"num_processed"
+#define NCONVERTED_CH_NAME	"num_converted"
+
 static char msg[PAGE_SIZE];
-static ssize_t msg_size;
-static struct proc_dir_entry *ent;
+static char buffer[PAGE_SIZE] = "0";
+static ssize_t msg_size, buf_size;
+static struct proc_dir_entry *uc_dir;
+static struct proc_dir_entry *ent_conv, *ent_stat_calls,
+			*ent_stat_proc, *ent_stat_conv;
+
+static int total_calls;
+static int total_procedded_ch;
+static int total_converted_ch;
 
 static char to_upper(const char ch)
 {
+	total_procedded_ch++;
 	if (ch >= 97 && ch <= 122) {
+		total_converted_ch++;
 		return (ch - 32);
 	}
 	return ch;
@@ -32,6 +47,24 @@ static int to_uppercase(char *input_str, size_t count)
 
 }
 
+static ssize_t uc_stat_calls_read(struct file *file, char __user *pbuf,
+					size_t count, loff_t *ppos)
+{
+
+	ssize_t num, not_copied;
+
+	pr_info("uppercase_conv_module: Number of calls: %d\n", total_calls);
+	num = min_t(ssize_t, buf_size, count);
+	if (num) {
+		not_copied = copy_to_user(pbuf, buffer, num);
+		num -= not_copied;
+	}
+
+	buf_size = 0; /* Indicate EOF on next read */
+
+	return num;
+}
+
 static ssize_t uc_conv_write(struct file *file, const char __user *pbuf,
 						size_t count, loff_t *ppos)
 {
@@ -48,7 +81,8 @@ static ssize_t uc_conv_write(struct file *file, const char __user *pbuf,
 	}
 
 	pr_info("uppercase_conv_module: Output message: %s\n", msg);
-
+	total_calls++;
+	buf_size = sprintf(buffer, "Number of calls: %d\n", total_calls);
 	return msg_size;
 }
 
@@ -69,6 +103,11 @@ static ssize_t uc_conv_read(struct file *file, char __user *pbuf,
 	return num;
 }
 
+static struct file_operations uc_stat_calls = {
+	.owner = THIS_MODULE,
+	.read = uc_stat_calls_read,
+};
+
 static struct file_operations uc_ops = {
 	.owner = THIS_MODULE,
 	.read = uc_conv_read,
@@ -77,15 +116,36 @@ static struct file_operations uc_ops = {
 
 static int uppercase_converter_module_init(void)
 {
+
 	pr_info("uppercase_conv_module: module loading\n");
-	ent = proc_create("uc_conv_procfs", 0666, NULL, &uc_ops);
+	uc_dir = proc_mkdir(DIR_NAME, NULL);
+	if (uc_dir == NULL) {
+		pr_err("uppercase_conv_module: error creating procfs directory\n");
+		return -ENOMEM;
+	}
+
+	ent_conv = proc_create(CONVETER_NAME, 0666, uc_dir, &uc_ops);
+	if (ent_conv == NULL) {
+		pr_err("uppercase_conv_module: error creating procfs entry 1\n");
+		proc_remove(uc_dir);
+		return -ENOMEM;
+	}
+
+	ent_stat_calls = proc_create(NCALLS_NAME, 0444, uc_dir, &uc_stat_calls);
+	if (ent_stat_calls == NULL) {
+		pr_err("uppercase_conv_module: error creating procfs entry 1\n");
+		proc_remove(uc_dir);
+		return -ENOMEM;
+	}
+
 	pr_info("uppercase_conv_module: module loaded\n");
+
 	return 0;
 }
 
 static void uppercase_converter_module_exit(void)
 {
-	proc_remove(ent);
+	proc_remove(uc_dir);
 	pr_info("uppercase_conv_module: module terminated");
 }
 
@@ -93,6 +153,6 @@ module_init(uppercase_converter_module_init);
 module_exit(uppercase_converter_module_exit);
 
 MODULE_AUTHOR("Vitalii Pudov <pudov@ukr.net>");
-MODULE_DESCRIPTION("Uppercase convertor, Kernel module");
+MODULE_DESCRIPTION("Uppercase converter, Kernel module");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
