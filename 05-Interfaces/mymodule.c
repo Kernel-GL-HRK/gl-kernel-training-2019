11a183204c8910474e799ae8e51dc121c80fd7d3
Dmitry Domnin <dmitry8728@gmail.com>
2019-11-03 20:53:40 +0200

    05-Interfaces: Create sysfs attributes
    Signed-off-by: Dmitry Domnin <dmitry8728@gmail.com>

--- 05-Interfaces/mymodule.c
+++ 05-Interfaces/mymodule.c
@@ -2,16 +2,81 @@
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/err.h>
+#include <linux/uaccess.h>
+#include <linux/ctype.h>
+
+#define CLASS_NAME	"my_module"
+#define CONV_FILE_NAME	"string-conv"
+#define LOG_FILE_NAME	"log"
+
+static struct class *attr_class = 0;
+
+static ssize_t rw_show(struct class *class, struct class_attribute *attr,
+	char *buf)
+{
+	int ret = 0;
+	pr_info("mymodule-sysfs: read file: %s\n", attr->attr.name);
+
+	return ret;
+}
+
+static ssize_t rw_store(struct class *class, struct class_attribute *attr,
+	const char *buf, size_t count)
+{
+	pr_info("mymodule-sysfs: write file: %s\n", attr->attr.name);
+	pr_info("mymodule-sysfs: write count bytes: %d\n", count);
+
+	return count;
+}
+
+/*CLASS_ATTR_RW(rw);*/
+struct class_attribute class_attr_sconv = {
+	.attr = { .name = CONV_FILE_NAME, .mode = 0666 },
+	.show	= rw_show,
+	.store	= rw_store
+};
+
+struct class_attribute class_attr_log = {
+	.attr = { .name = LOG_FILE_NAME, .mode = 0666 },
+	.show	= rw_show,
+	.store	= rw_store
+};
 
 static int __init init_mod(void)
 {
-	pr_info("mymodule: module loaded\n");
+	int ret;
+
+	attr_class = class_create(THIS_MODULE, CLASS_NAME);
+	if (attr_class == NULL) {
+		pr_err("mymodule-sysfs: error creating sysfs class\n");
+		return -ENOMEM;
+	}
+
+	ret = class_create_file(attr_class, &class_attr_sconv);
+	if (ret) {
+		pr_err("mymodule-sysfs: error creating sysfs class attribute %s\n", CONV_FILE_NAME);
+		class_destroy(attr_class);
+		return ret;
+	}
+
+	ret = class_create_file(attr_class, &class_attr_log);
+	if (ret) {
+		pr_err("mymodule-sysfs: error creating sysfs class attribute %s\n", LOG_FILE_NAME);
+		class_remove_file(attr_class, &class_attr_sconv);	
+		class_destroy(attr_class);
+		return ret;
+	}
+
+	pr_info("mymodule-sysfs: module loaded\n");
 	return 0;
 }
 
 static void __exit exit_mod(void)
 {
-	pr_info("mymodule: module removed\n");
+	class_remove_file(attr_class, &class_attr_log);
+	class_remove_file(attr_class, &class_attr_sconv);
+	class_destroy(attr_class);
+	pr_info("mymodule-sysfs: module removed\n");
 }
 
 module_init(init_mod);
@@ -19,5 +84,5 @@ module_exit(exit_mod);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dmitry Domnin");
-MODULE_DESCRIPTION("A simple Linux driver.");
+MODULE_DESCRIPTION("A simple sysfs module");
 MODULE_VERSION("0.1");
