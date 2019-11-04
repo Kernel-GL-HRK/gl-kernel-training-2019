f1b4a59afd9f8cb5a6e1233a13ac56d0204ea684
Vitalii Pudov <pudov@ukr.net>
2019-11-04 11:33:47 +0200

    05-Interfaces: Add calls statistics
    Implementation of number of function calls show
    
    Signed-off-by: Vitalii Pudov <pudov@ukr.net>

--- 05-Interfaces/sysfs/lowercase_conv_module.c
+++ 05-Interfaces/sysfs/lowercase_conv_module.c
@@ -6,6 +6,8 @@
 
 static char msg[PAGE_SIZE];
 
+static int total_calls;
+
 static char ch_to_lower(const char ch)
 {
 
@@ -29,11 +31,20 @@ static int to_lowercase(char *input_str, size_t count)
 
 }
 
+static ssize_t calls_show(struct class *class,
+				struct class_attribute *attr,
+						char *buf)
+{
+	pr_info("lowercase_conv_module: num of calls is %d\n", total_calls);
+	sprintf(buf, "num of calls: %d\n", total_calls);
+	return strlen(buf);
+}
+
 static ssize_t conv_string_show(struct class *class,
 				struct class_attribute *attr,
 						char *buf)
 {
-	pr_info("lowercase_conv_module: output: %s %s\n", msg, buf);
+	pr_info("lowercase_conv_module: output: %s\n", msg);
 	strcpy(buf, msg);
 	return strlen(buf);
 }
@@ -42,6 +53,7 @@ static ssize_t conv_string_store(struct class *class,
 					struct class_attribute *attr,
 					const char *buf, size_t count)
 {
+	total_calls++;
 	pr_info("lowercase_conv_module: input: %s\n", buf);
 	strncpy(msg, buf, strlen(buf));
 	if (to_lowercase(msg, count) < 0) {
@@ -56,6 +68,11 @@ struct class_attribute class_attr_conv_string = {
 	.attr = { .name = "conv_string", .mode = 0666 },
 	.show	= conv_string_show,
 	.store	= conv_string_store
+};
+
+struct class_attribute class_attr_stat_calls = {
+	.attr = { .name = "num_calls", .mode = 0444 },
+	.show	= calls_show,
 };
 
 static struct class *attr_class = 0;
@@ -73,6 +90,13 @@ static int lowercase_converter_module_init(void)
 	}
 
 	ret = class_create_file(attr_class, &class_attr_conv_string);
+	if (ret) {
+		pr_err("lowercase_conv_module: error creating sysfs class attribute\n");
+		class_destroy(attr_class);
+		return ret;
+	}
+
+	ret = class_create_file(attr_class, &class_attr_stat_calls);
 	if (ret) {
 		pr_err("lowercase_conv_module: error creating sysfs class attribute\n");
 		class_destroy(attr_class);
