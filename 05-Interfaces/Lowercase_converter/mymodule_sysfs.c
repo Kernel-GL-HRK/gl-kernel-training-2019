d9e7234b83d0d3c4efd10d224d4c4bcba3ff15cb
Anton <ad742452@gmail.com>
2019-11-08 06:00:48 +0200

    05-Interfaces: Add statistics visualization solution for sysfs
    Interface sysfs was using to implement statistics visualization
    
    Signed-off-by: Anton <ad742452@gmail.com>

--- 05-Interfaces/Lowercase_converter/mymodule_sysfs.c
+++ 05-Interfaces/Lowercase_converter/mymodule_sysfs.c
@@ -5,6 +5,11 @@
 
 char arr[PAGE_SIZE];
 
+static int statTotalReadCalls = 0;
+static int statTotalWriteCalls = 0;
+static int amountCharactersProcessed = 0;
+static int amountCharactersConverted = 0;
+
 // routines
 static void lowercaseConvert(char *pbuf)
 {
@@ -14,8 +19,10 @@ static void lowercaseConvert(char *pbuf)
 		if(pbuf[i] >= 'A' && pbuf[i] <= 'Z')
 		{
 			pbuf[i] = pbuf[i] - 'A' + 'a';
+			amountCharactersConverted++;
 		}
 		i++;
+		amountCharactersProcessed++; // statistic	
 	}
 }
 
@@ -24,6 +31,8 @@ static ssize_t param_show(struct class *class, struct class_attribute *attr, cha
 	pr_info("mymodule: read data");
 
 	strcpy(buf, arr);
+
+	statTotalReadCalls++;
 
 	return strlen(buf);
 }
@@ -43,15 +52,48 @@ static ssize_t param_store(struct class *class, struct class_attribute *attr, co
 		arr[i] = buf[i];
 	arr[sizeToCopy] = 0;
 
+	statTotalWriteCalls++;
+
 	lowercaseConvert(arr); // convert
 
 	return count;
+}
+
+static ssize_t stat_show(struct class *class, struct class_attribute *attr, char *buf)
+{
+	char buff[500] = "";
+	char buffstat[40];
+
+	strcat(buff, "-----------------------------------\n");
+	strcat(buff, "| Lowercase convrting statistic   |\n");
+	strcat(buff, "-----------------------------------\n");
+	sprintf(buffstat, "| %-23s | %5d |\n", "Total write calls", statTotalWriteCalls);
+	strcat(buff, buffstat);	
+	strcat(buff, "-----------------------------------\n");
+	sprintf(buffstat, "| %-23s | %5d |\n", "Total read calls", statTotalReadCalls);
+	strcat(buff, buffstat);	
+	strcat(buff, "-----------------------------------\n");
+	sprintf(buffstat, "| %-23s | %5d |\n", "Characters processed", amountCharactersProcessed);
+	strcat(buff, buffstat);	
+	strcat(buff, "-----------------------------------\n");
+	sprintf(buffstat, "| %-23s | %5d |\n", "Characters converted", amountCharactersConverted);
+	strcat(buff, buffstat);	
+	strcat(buff, "-----------------------------------\n");	
+	
+	strcpy(buf, buff);
+	
+	return strlen(buf);
 }
 
 struct class_attribute class_attr_param = {
 	.attr = { .name = "param", .mode = 0666 },
 	.show	= param_show,
 	.store	= param_store
+};
+
+struct class_attribute class_attr_stat = {
+	.attr = { .name = "stat", .mode = 0666 },
+	.show	= stat_show,	
 };
 
 static struct class *attr_class = 0;
@@ -73,12 +115,21 @@ static int mymodule_init(void)
 		return ret;
 	}
 
+	ret = class_create_file(attr_class, &class_attr_stat);
+	if (ret) {
+		pr_err("mymodule: error creating sysfs class attribute\n");
+		class_remove_file(attr_class, &class_attr_param);
+		class_destroy(attr_class);
+		return ret;
+	}
+
 	pr_info("mymodule: module loaded\n");
 	return 0;
 }
 
 static void mymodule_exit(void)
 {
+	class_remove_file(attr_class, &class_attr_stat);
 	class_remove_file(attr_class, &class_attr_param);
 	class_destroy(attr_class);
 
