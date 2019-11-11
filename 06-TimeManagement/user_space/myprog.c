8aff412ac10a8befc2053ff99ff69d97fb9a45e8
Dmitry Domnin <dmitry8728@gmail.com>
2019-11-11 17:38:51 +0200

    06-TimeManagement: Add the result of the program (log)
    Signed-off-by: Dmitry Domnin <dmitry8728@gmail.com>

--- 06-TimeManagement/user_space/myprog.c
+++ 06-TimeManagement/user_space/myprog.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/time.h>
 #include <fcntl.h>
+
 
 #define INPUT_ATTR "/sys/class/my_time_mod/absolute-time"
 
@@ -14,24 +16,26 @@ int main(void)
 {
 	fd = open(INPUT_ATTR, O_RDONLY);
 
-        if (fd < 0) {
-                fprintf(stderr, "Error opening %s for reading\n", INPUT_ATTR);
-                exit(EXIT_FAILURE);
-        }
+	if (fd < 0) {
+		fprintf(stderr, "Error opening %s for reading\n",
+			INPUT_ATTR);
+		exit(EXIT_FAILURE);
+	}
 
-	ret = read (fd, str, sizeof(str));
-	if(ret < 0) {
-                fprintf(stderr, "Read error %s\n", INPUT_ATTR);
-                exit(EXIT_FAILURE);
-        }
+	ret = read(fd, str, sizeof(str));
+	if (ret < 0) {
+		fprintf(stderr, "Read error %s\n", INPUT_ATTR);
+		exit(EXIT_FAILURE);
+	}
 
 	ret = gettimeofday(&t, NULL);
 	if (ret < 0)
 		exit(EXIT_FAILURE);
 
-	printf("User-space get absolute time: %ld.%ld\n", t.tv_sec, t.tv_usec);
+	printf("User-space get absolute time: %ld.%06ld\n",
+		t.tv_sec, t.tv_usec);
 	printf("Kernel-space get absolute time: %s\n", str);
 
 	close(fd);
-        return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
